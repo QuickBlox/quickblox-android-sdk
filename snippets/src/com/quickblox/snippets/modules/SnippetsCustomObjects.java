59c62daab98f0d594e078655fd5d3277c3771f22
Igor Khomenko <igor@quickblox.com>
2013-01-03 17:33:02 +0200

    fixes for CO

--- snippets/src/com/quickblox/snippets/modules/SnippetsCustomObjects.java
+++ snippets/src/com/quickblox/snippets/modules/SnippetsCustomObjects.java
@@ -3,6 +3,10 @@ package com.quickblox.snippets.modules;
 import android.content.Context;
 import com.quickblox.core.QBCallbackImpl;
 import com.quickblox.core.result.Result;
+import com.quickblox.internal.core.helper.GenericQueryRule;
+import com.quickblox.internal.module.custom.request.QBCustomObjectRequestBuilder;
+import com.quickblox.internal.module.custom.request.QBCustomObjectUpdateBuilder;
+import com.quickblox.internal.module.custom.request.QueryRule;
 import com.quickblox.module.custom.QBCustomObjects;
 import com.quickblox.module.custom.model.QBCustomObject;
 import com.quickblox.module.custom.result.QBCustomObjectLimitedResult;
@@ -25,6 +29,8 @@ public class SnippetsCustomObjects extends Snippets {
     String className = "hero";
     String fieldHealth = "health";
     String fieldPower = "power";
+    String fieldTags = "tags";
+
 
     public SnippetsCustomObjects(Context context) {
         super(context);
@@ -32,28 +38,13 @@ public class SnippetsCustomObjects extends Snippets {
         snippets.add(createCustomObject);
         snippets.add(getCustomObjectById);
         snippets.add(deleteCustomObject);
-        snippets.add(getCustomObjects);
+
         snippets.add(updateCustomObject);
+        snippets.add(updateCustomObjectWithSpecialUpdateParameters);
+
+        snippets.add(getCustomObjects);
+        snippets.add(getCustomObjectsWithParams);
     }
-
-    Snippet getCustomObjects = new Snippet("get objects") {
-        @Override
-        public void execute() {
-            QBCustomObjects.getObjects(className, new QBCallbackImpl() {
-                @Override
-                public void onComplete(Result result) {
-                    if (result.isSuccess()) {
-                        QBCustomObjectLimitedResult coresult = (QBCustomObjectLimitedResult) result;
-
-                        ArrayList<QBCustomObject> co = coresult.getCustomObjects();
-                        System.out.println(">>> custom object list: " + co.toString());
-                    } else {
-                        handleErrors(result);
-                    }
-                }
-            });
-        }
-    };
 
     Snippet createCustomObject = new Snippet("create object") {
         @Override
@@ -61,8 +52,11 @@ public class SnippetsCustomObjects extends Snippets {
             QBCustomObject customObject = new QBCustomObject(className);
             customObject.put(fieldHealth, 99);
             customObject.put(fieldPower, 123.45);
+            ArrayList<String> tags = new ArrayList<String>();
+            tags.add("man");
+            tags.add("car");
+            customObject.put(fieldTags, tags);
             customObject.setParentId("50d9bf2d535c12344701c43a");
-
 
             QBCustomObjects.createObject(customObject, new QBCallbackImpl() {
                 @Override
@@ -84,7 +78,7 @@ public class SnippetsCustomObjects extends Snippets {
     Snippet getCustomObjectById = new Snippet("get object") {
         @Override
         public void execute() {
-            QBCustomObject customObject = new QBCustomObject(className, "50e3f8c7535c126073000d52");
+            QBCustomObject customObject = new QBCustomObject(className, "50e59f81535c121c660015fd");
 
             QBCustomObjects.getObject(customObject, new QBCallbackImpl() {
                 @Override
@@ -105,7 +99,7 @@ public class SnippetsCustomObjects extends Snippets {
     Snippet deleteCustomObject = new Snippet("delete object") {
         @Override
         public void execute() {
-            QBCustomObject customObject = new QBCustomObject(className, "af3514342afbbb3555");
+            QBCustomObject customObject = new QBCustomObject(className, "50e59dc7535c121f6600155b");
 
             QBCustomObjects.deleteObject(customObject, new QBCallbackImpl() {
                 @Override
@@ -125,11 +119,12 @@ public class SnippetsCustomObjects extends Snippets {
         public void execute() {
             QBCustomObject co = new QBCustomObject();
             co.setClassName(className);
+            co.setCustomObjectId("50e59f81535c121c660015fd");
+            //
             HashMap<String, Object> fields = new HashMap<String, Object>();
             fields.put(fieldPower, 1);
             fields.put(fieldHealth, 10);
             co.setFields(fields);
-            co.setCustomObjectId("50e3f85f535c123376000d31");
 
             QBCustomObjects.updateObject(co, new QBCallbackImpl() {
                 @Override
@@ -137,10 +132,85 @@ public class SnippetsCustomObjects extends Snippets {
                     if (result.isSuccess()) {
                         QBCustomObjectResult updateResult = (QBCustomObjectResult) result;
 
-                        System.out.println(">>> updatedAt: " + updateResult.getCustomObject().getUpdatedAt());
-                        System.out.println(">>> createdAt: " + updateResult.getCustomObject().getCreatedAt());
+                        System.out.println(">>> co : " + updateResult.getCustomObject().toString());
+                    } else {
+                        handleErrors(result);
+                    }
+                }
+            });
+        }
+    };
+
+    Snippet updateCustomObjectWithSpecialUpdateParameters = new Snippet("update object", "with special update parameters") {
+        @Override
+        public void execute() {
+            QBCustomObject co = new QBCustomObject();
+            co.setClassName(className);
+            co.setCustomObjectId("50e59f81535c121c660015fd");
+            //
+            HashMap<String, Object> fields = new HashMap<String, Object>();
+            fields.put(fieldHealth, 100);
+            co.setFields(fields);
+
+            // special update operators
+            QBCustomObjectUpdateBuilder updateBuilder = new QBCustomObjectUpdateBuilder();
+            updateBuilder.push(fieldTags, "girl");
+            updateBuilder.push(fieldTags, "circle");
+
+            QBCustomObjects.updateObject(co, updateBuilder, new QBCallbackImpl() {
+                @Override
+                public void onComplete(Result result) {
+                    if (result.isSuccess()) {
+                        QBCustomObjectResult updateResult = (QBCustomObjectResult) result;
 
                         System.out.println(">>> co : " + updateResult.getCustomObject().toString());
+                    } else {
+                        handleErrors(result);
+                    }
+                }
+            });
+        }
+    };
+
+
+    Snippet getCustomObjects = new Snippet("get objects") {
+        @Override
+        public void execute() {
+            QBCustomObjects.getObjects(className, new QBCallbackImpl() {
+                @Override
+                public void onComplete(Result result) {
+                    if (result.isSuccess()) {
+                        QBCustomObjectLimitedResult coresult = (QBCustomObjectLimitedResult) result;
+
+                        ArrayList<QBCustomObject> co = coresult.getCustomObjects();
+                        System.out.println(">>> custom object list: " + co.toString());
+                    } else {
+                        handleErrors(result);
+                    }
+                }
+            });
+        }
+    };
+
+    Snippet getCustomObjectsWithParams = new Snippet("get objects", "with parameters, search operators, aggregation operators") {
+        @Override
+        public void execute() {
+
+            QBCustomObjectRequestBuilder requestBuilder = new QBCustomObjectRequestBuilder();
+
+            ArrayList<GenericQueryRule> rules =  new ArrayList<GenericQueryRule>();
+            QueryRule rule = new QueryRule(fieldPower, QueryRule.EQ, "345");
+            rules.add(rule);
+            requestBuilder.setRules(rules);
+
+            QBCustomObjects.getObjects(className, requestBuilder, new QBCallbackImpl() {
+                @Override
+                public void onComplete(Result result) {
+                    if (result.isSuccess()) {
+                        QBCustomObjectLimitedResult coresult = (QBCustomObjectLimitedResult) result;
+
+                        ArrayList<QBCustomObject> co = coresult.getCustomObjects();
+                        System.out.println(">>> custom object list: " + co.toString());
                     } else {
                         handleErrors(result);
                     }
