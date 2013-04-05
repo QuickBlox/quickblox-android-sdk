308cee2ee45dc230dca1bc41f70658de0010a9eb
nickolas <nikolay.dymura@injoit.com>
2013-04-05 10:37:41 +0300

    load tabs logic

--- sample-chat-v2/src/com/quickblox/chat_v2/activitys/MainTabActivity.java
+++ sample-chat-v2/src/com/quickblox/chat_v2/activitys/MainTabActivity.java
@@ -1,5 +1,7 @@
 package com.quickblox.chat_v2.activitys;
 
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 import com.quickblox.chat_v2.R;
 import com.quickblox.chat_v2.R.layout;
 import com.quickblox.chat_v2.R.menu;
@@ -7,20 +9,31 @@ import com.quickblox.chat_v2.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import com.quickblox.chat_v2.fragment.ContactsFragment;
+import com.quickblox.chat_v2.fragment.DialogsFragment;
+import com.quickblox.chat_v2.fragment.ProfileFragment;
+import com.quickblox.chat_v2.fragment.RoomsFragment;
 
-public class MainTabActivity extends Activity {
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main_tab);
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_tab, menu);
-		return true;
-	}
+public class MainTabActivity extends FragmentActivity {
+
+    private FragmentTabHost mTabHost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.fragment_tabs);
+        mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+
+        mTabHost.addTab(mTabHost.newTabSpec("dialogs").setIndicator(getString(R.string.dialogs)),
+                DialogsFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("rooms").setIndicator(getString(R.string.rooms)),
+                RoomsFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("contacts").setIndicator(getString(R.string.contacts)),
+                ContactsFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("profile").setIndicator(getString(R.string.profile)),
+                ProfileFragment.class, null);
+    }
 	
 }
---
+++ sample-chat-v2/src/com/quickblox/chat_v2/fragment/ContactsFragment.java
@@ -0,0 +1,12 @@
+package com.quickblox.chat_v2.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nickolas
+ * Date: 05.04.13
+ * Time: 9:58
+ */
+public class ContactsFragment extends Fragment {
+}
