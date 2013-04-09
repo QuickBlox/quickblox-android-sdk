91c4fa3337ac67bfa6b814835768eeeb4d41f47f
Andrew Dmitrenko <grenka487@gmail.com>
2013-04-09 17:29:30 +0300

    fix for MAinActivity

--- sample-chat-v2/src/com/quickblox/chat_v2/activitys/MainActivity.java
+++ sample-chat-v2/src/com/quickblox/chat_v2/activitys/MainActivity.java
@@ -8,8 +8,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.text.TextUtils;
 import android.widget.TabHost;
-import android.widget.Toast;
-
 import com.quickblox.chat_v2.R;
 import com.quickblox.chat_v2.fragment.ContactsFragment;
 import com.quickblox.chat_v2.fragment.DialogsFragment;
@@ -18,7 +16,6 @@ import com.quickblox.chat_v2.fragment.RoomsFragment;
 import com.quickblox.chat_v2.utils.SharedPreferencesHelper;
 import com.quickblox.core.QBCallbackImpl;
 import com.quickblox.core.result.Result;
-import com.quickblox.module.auth.model.QBProvider;
 import com.quickblox.module.chat.QBChat;
 import com.quickblox.module.chat.xmpp.LoginListener;
 import com.quickblox.module.users.QBUsers;
@@ -29,147 +26,130 @@ import com.quickblox.module.users.result.QBUserResult;
  * Created with IntelliJ IDEA. User: Andrew Dmitrenko Date: 4/8/13 Time: 1:34 PM
  */
 public class MainActivity extends FragmentActivity implements TabHost.OnTabChangeListener {
-	
-	private static Context context;
-	private static FragmentManager fragmentManager;
-	private TabHost mTabHost;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_activity);
-		context = getBaseContext();
-		fragmentManager = getSupportFragmentManager();
-		
-		if (TextUtils.isEmpty(SharedPreferencesHelper.getAccessToken()) && TextUtils.isEmpty(SharedPreferencesHelper.getLogin())) {
-			System.out.println("tokien"+SharedPreferencesHelper.getAccessToken());
-			loadSplashScreen();
-		} else {
-			if (TextUtils.isEmpty(SharedPreferencesHelper.getLogin())) {
-				authWithSocialProvider();
-			} else {
-				authWithUser();
-			}
-			setupTabs();
-		}
-	}
-	
-	public static Context getContext() {
-		return context;
-	}
-	
-	public static FragmentManager getSupportFragmentActivityManager() {
-		return fragmentManager;
-	}
-	
-	public static void loadDialogScreen() {
-		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-		fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-		DialogsFragment dialogsFragment = new DialogsFragment();
-		fragmentTransaction.replace(R.id.main, dialogsFragment);
-		fragmentTransaction.commitAllowingStateLoss();
-	}
-	
-	public static void loadRoomsScreen() {
-		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-		fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-		RoomsFragment roomsFragment = new RoomsFragment();
-		fragmentTransaction.replace(R.id.main, roomsFragment);
-		fragmentTransaction.commitAllowingStateLoss();
-	}
-	
-	public static void loadContactsScreen() {
-		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-		fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-		ContactsFragment contactsFragment = new ContactsFragment();
-		fragmentTransaction.replace(R.id.main, contactsFragment);
-		fragmentTransaction.commitAllowingStateLoss();
-	}
-	
-	public static void loadProfileScreen() {
-		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-		fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-		ProfileFragment profileFragment = new ProfileFragment();
-		fragmentTransaction.replace(R.id.main, profileFragment);
-		fragmentTransaction.commitAllowingStateLoss();
-	}
-	
-	private void loadSplashScreen() {
-		Intent intent = new Intent(getContext(), SplashActivity.class);
-		startActivity(intent);
-		finish();
-	}
-	
-	private void setupTabs() {
-		TabHost.TabSpec spec; // Resusable TabSpec for each tab
-		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-		mTabHost.setOnTabChangedListener(this);
-		mTabHost.setup(); // you must call this before adding your tabs!
-		spec = mTabHost.newTabSpec(getString(R.string.TAB_DIALOGS_TITLE)).setIndicator(getString(R.string.maintab_tab_1)).setContent(R.id.tab_dialogs);
-		mTabHost.addTab(spec);
-		spec = mTabHost.newTabSpec(getString(R.string.TAB_ROOMS_TITLE)).setIndicator(getString(R.string.maintab_tab_2)).setContent(R.id.tab_rooms);
-		mTabHost.addTab(spec);
-		spec = mTabHost.newTabSpec(getString(R.string.TAB_CONTACTS_TITLE)).setIndicator(getString(R.string.maintab_tab_3)).setContent(R.id.tab_contacts);
-		mTabHost.addTab(spec);
-		spec = mTabHost.newTabSpec(getString(R.string.TAB_PROFILE_TITLE)).setIndicator(getString(R.string.maintab_tab_4)).setContent(R.id.tab_profiles);
-		mTabHost.addTab(spec);
-	}
-	
-	private void updateTab(String tabTitle) {
-		if (tabTitle.equals(getString(R.string.TAB_DIALOGS_TITLE))) {
-			loadDialogScreen();
-		} else if (tabTitle.equals(getString(R.string.TAB_ROOMS_TITLE))) {
-			loadRoomsScreen();
-		} else if (tabTitle.equals(getString(R.string.TAB_CONTACTS_TITLE))) {
-			loadContactsScreen();
-		} else if (tabTitle.equals(getString(R.string.TAB_PROFILE_TITLE))) {
-			loadProfileScreen();
-		}
-	}
-	
-	private void authWithSocialProvider() {
-		QBUsers.signInUsingSocialProvider(QBProvider.FACEBOOK, SharedPreferencesHelper.getAccessToken(), null, new QBCallbackImpl() {
-			@Override
-			public void onComplete(Result result) {
-				if (result.isSuccess()) {
-					QBUser qbUser = ((QBUserResult) result).getUser();
-					SharedPreferencesHelper.setPassword(qbUser.getPassword());
-					signInChat(qbUser);
-				}
-			}
-		});
-	}
-	
-	private void authWithUser() {
-		QBUsers.signIn(SharedPreferencesHelper.getLogin(), SharedPreferencesHelper.getPassword(), new QBCallbackImpl() {
-			@Override
-			public void onComplete(Result result) {
-				if (result.isSuccess()) {
-					signInChat(((QBUserResult) result).getUser());
-				}
-			}
-		});
-	}
-	
-	private void signInChat(QBUser qbUser) {
-		
-		QBChat.loginWithUser(qbUser, new LoginListener() {
-			
-			@Override
-			public void onLoginError() {
-				
-			}
-			
-			@Override
-			public void onLoginSuccess() {
-				
-			}
-			
-		});
-	}
-	
-	@Override
-	public void onTabChanged(String tabTitle) {
-		updateTab(tabTitle);
-	}
+
+    private static Context context;
+    private static FragmentManager fragmentManager;
+    private TabHost mTabHost;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        context = getBaseContext();
+        fragmentManager = getSupportFragmentManager();
+
+        if (TextUtils.isEmpty(SharedPreferencesHelper.getLogin())) {
+            loadSplashScreen();
+        } else {
+            authWithUser();
+            setupTabs();
+        }
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+
+    public static FragmentManager getSupportFragmentActivityManager() {
+        return fragmentManager;
+    }
+
+    public static void loadDialogScreen() {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        DialogsFragment dialogsFragment = new DialogsFragment();
+        fragmentTransaction.replace(R.id.main, dialogsFragment);
+        fragmentTransaction.commitAllowingStateLoss();
+    }
+
+    public static void loadRoomsScreen() {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        RoomsFragment roomsFragment = new RoomsFragment();
+        fragmentTransaction.replace(R.id.main, roomsFragment);
+        fragmentTransaction.commitAllowingStateLoss();
+    }
+
+    public static void loadContactsScreen() {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ContactsFragment contactsFragment = new ContactsFragment();
+        fragmentTransaction.replace(R.id.main, contactsFragment);
+        fragmentTransaction.commitAllowingStateLoss();
+    }
+
+    public static void loadProfileScreen() {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ProfileFragment profileFragment = new ProfileFragment();
+        fragmentTransaction.replace(R.id.main, profileFragment);
+        fragmentTransaction.commitAllowingStateLoss();
+    }
+
+    private void loadSplashScreen() {
+        Intent intent = new Intent(getContext(), SplashActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void setupTabs() {
+        TabHost.TabSpec spec; // Resusable TabSpec for each tab
+        mTabHost = (TabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setOnTabChangedListener(this);
+        mTabHost.setup(); // you must call this before adding your tabs!
+        spec = mTabHost.newTabSpec(getString(R.string.TAB_DIALOGS_TITLE)).setIndicator(getString(R.string.maintab_tab_1)).setContent(R.id.tab_dialogs);
+        mTabHost.addTab(spec);
+        spec = mTabHost.newTabSpec(getString(R.string.TAB_ROOMS_TITLE)).setIndicator(getString(R.string.maintab_tab_2)).setContent(R.id.tab_rooms);
+        mTabHost.addTab(spec);
+        spec = mTabHost.newTabSpec(getString(R.string.TAB_CONTACTS_TITLE)).setIndicator(getString(R.string.maintab_tab_3)).setContent(R.id.tab_contacts);
+        mTabHost.addTab(spec);
+        spec = mTabHost.newTabSpec(getString(R.string.TAB_PROFILE_TITLE)).setIndicator(getString(R.string.maintab_tab_4)).setContent(R.id.tab_profiles);
+        mTabHost.addTab(spec);
+    }
+
+    private void updateTab(String tabTitle) {
+        if (tabTitle.equals(getString(R.string.TAB_DIALOGS_TITLE))) {
+            loadDialogScreen();
+        } else if (tabTitle.equals(getString(R.string.TAB_ROOMS_TITLE))) {
+            loadRoomsScreen();
+        } else if (tabTitle.equals(getString(R.string.TAB_CONTACTS_TITLE))) {
+            loadContactsScreen();
+        } else if (tabTitle.equals(getString(R.string.TAB_PROFILE_TITLE))) {
+            loadProfileScreen();
+        }
+    }
+
+
+    private void authWithUser() {
+        QBUsers.signIn(SharedPreferencesHelper.getLogin(), SharedPreferencesHelper.getPassword(), new QBCallbackImpl() {
+            @Override
+            public void onComplete(Result result) {
+                if (result.isSuccess()) {
+                    signInChat(((QBUserResult) result).getUser());
+                }
+            }
+        });
+    }
+
+    private void signInChat(QBUser qbUser) {
+
+        QBChat.loginWithUser(qbUser, new LoginListener() {
+
+            @Override
+            public void onLoginError() {
+
+            }
+
+            @Override
+            public void onLoginSuccess() {
+
+            }
+
+        });
+    }
+
+    @Override
+    public void onTabChanged(String tabTitle) {
+        updateTab(tabTitle);
+    }
 }
--- sample-chat-v2/src/com/quickblox/chat_v2/activitys/SplashActivity.java
+++ sample-chat-v2/src/com/quickblox/chat_v2/activitys/SplashActivity.java
@@ -12,9 +12,6 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.facebook.Session;
-import com.facebook.SessionState;
 import com.quickblox.chat_v2.R;
 import com.quickblox.chat_v2.fragment.SplashDialog;
 import com.quickblox.chat_v2.others.ChatApplication;
@@ -26,168 +23,171 @@ import com.quickblox.core.result.Result;
 import com.quickblox.internal.core.exception.BaseServiceException;
 import com.quickblox.internal.core.server.BaseService;
 import com.quickblox.module.auth.QBAuth;
-import com.quickblox.module.auth.model.QBProvider;
-import com.quickblox.module.users.QBUsers;
 import com.quickblox.module.users.model.QBUser;
 import com.quickblox.module.users.result.QBUserResult;
 
 public class SplashActivity extends FragmentActivity implements QBCallback, Session.StatusCallback {
-	
-	private DialogFragment quickBloxDialog;
-	private ChatApplication app;
-	private ProgressDialog progress;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		
-		setContentView(R.layout.activity_splash);
-		
-		Button facebookButton = (Button) findViewById(R.id.splash_facebook_button);
-		Button registrationButton = (Button) findViewById(R.id.splash_registration_button);
-		Button siginButton = (Button) findViewById(R.id.splash_sign_in_button);
-		
-		progress = ProgressDialog.show(this, getResources().getString(R.string.app_name), getResources().getString(R.string.splash_progressdialog), true);
-		
-		OnClickListener clickButtonListener = new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-				switch (v.getId()) {
-					case R.id.splash_facebook_button :
-						if (!isOnline()) { break;}
-						onFbClickLogin();
-						break;
-					
-					case R.id.splash_registration_button :
-						if (!isOnline()) { break;}
-						quickBloxDialog = new SplashDialog(true);
-						quickBloxDialog.show(getSupportFragmentManager(), null);
-						break;
-					
-					case R.id.splash_sign_in_button :
-						if (!isOnline()) { break;}
-						quickBloxDialog = new SplashDialog(false);
-						quickBloxDialog.show(getSupportFragmentManager(), null);
-						break;
-				
-				}
-			}
-			
-		};
-		
-		facebookButton.setOnClickListener(clickButtonListener);
-		registrationButton.setOnClickListener(clickButtonListener);
-		siginButton.setOnClickListener(clickButtonListener);
-		
-		if (isOnline()) {
-			
-			QBSettings.getInstance().fastConfigInit(getResources().getString(R.string.quickblox_app_id), getResources().getString(R.string.quickblox_auth_key),
-					getResources().getString(R.string.quickblox_auth_secret));
-			
-			app = ChatApplication.getInstance();
-			
-			QBAuth.createSession(new QBCallbackImpl() {
-				@Override
-				public void onComplete(Result arg0) {
-					progress.dismiss();
-				}
-			});
-			
-		} else {
-			progress.dismiss();
-		}
-		
-	}
-	
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-	}
-	
-	// FACEBOOK LOGIN
-	private void onFbClickLogin() {
-		
-		Session session = Session.getActiveSession();
-		
-		if (session == null) {
-			session = new Session(this);
-			
-			Session.setActiveSession(session);
-			if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
-				session.openForRead(new Session.OpenRequest(this).setCallback(this));
-			}
-			
-		}
-		if (!session.isOpened() && !session.isClosed()) {
-			session.openForRead(new Session.OpenRequest(this).setCallback(this));
-		}
-	}
-	
-	// QB CALLBACK
-	
-	@Override
-	public void onComplete(Result arg0) {
-	}
-	
-	@Override
-	public void onComplete(Result result, Object context) {
-		
-		if (result.isSuccess()) {
-			
-			QBUser qbUser = ((QBUserResult) result).getUser();
-			SharedPreferencesHelper.setLogin(qbUser.getLogin());
-			
-			if(context.toString().equals("social")){
-				try {
-					qbUser.setPassword(BaseService.getBaseService().getToken());
-				} catch (BaseServiceException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			else{
-				SharedPreferencesHelper.setPassword(qbUser.getPassword());
-			}
-			
-			
-			loadMainScreen();
-		}
 
-		
-	}
-	
-	// FACEBOOK CALLBACK
-	@Override
-	public void call(Session session, SessionState state, Exception exception) {
-		QBUsers.signInUsingSocialProvider(QBProvider.FACEBOOK, session.getAccessToken(), null, this, "social");
-		
-		System.out.println("token = "+session.getAccessToken());
-		
-		progress = ProgressDialog.show(this, getResources().getString(R.string.app_name), getResources().getString(R.string.splash_progressdialog), true);
-		
-	}
-	
-	private void loadMainScreen() {
-		Intent intent = new Intent(getBaseContext(), MainActivity.class);
-		startActivity(intent);
-		finish();
-	}
+    private DialogFragment quickBloxDialog;
+    private ChatApplication app;
+    private ProgressDialog progress;
 
-	
-	// INTERNET REVIEW
-	public boolean isOnline() {
-		ConnectivityManager cm = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
-		NetworkInfo netInfo = cm.getActiveNetworkInfo();
-		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
-			return true;
-		}
-		Toast.makeText(SplashActivity.this, getResources().getString(R.string.splash_internet_error), Toast.LENGTH_LONG).show();
-		return false;
-	}
-	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_splash);
+
+        Button facebookButton = (Button) findViewById(R.id.splash_facebook_button);
+        Button registrationButton = (Button) findViewById(R.id.splash_registration_button);
+        Button siginButton = (Button) findViewById(R.id.splash_sign_in_button);
+
+        progress = ProgressDialog.show(this, getResources().getString(R.string.app_name), getResources().getString(R.string.splash_progressdialog), true);
+
+        OnClickListener clickButtonListener = new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                switch (v.getId()) {
+                    case R.id.splash_facebook_button:
+                        if (!isOnline()) {
+                            break;
+                        }
+                        onFbClickLogin();
+                        break;
+
+                    case R.id.splash_registration_button:
+                        if (!isOnline()) {
+                            break;
+                        }
+                        quickBloxDialog = new SplashDialog(true);
+                        quickBloxDialog.show(getSupportFragmentManager(), null);
+                        break;
+
+                    case R.id.splash_sign_in_button:
+                        if (!isOnline()) {
+                            break;
+                        }
+                        quickBloxDialog = new SplashDialog(false);
+                        quickBloxDialog.show(getSupportFragmentManager(), null);
+                        break;
+
+                }
+            }
+
+        };
+
+        facebookButton.setOnClickListener(clickButtonListener);
+        registrationButton.setOnClickListener(clickButtonListener);
+        siginButton.setOnClickListener(clickButtonListener);
+
+        if (isOnline()) {
+
+            QBSettings.getInstance().fastConfigInit(getResources().getString(R.string.quickblox_app_id), getResources().getString(R.string.quickblox_auth_key),
+                    getResources().getString(R.string.quickblox_auth_secret));
+
+            app = ChatApplication.getInstance();
+
+            QBAuth.createSession(new QBCallbackImpl() {
+                @Override
+                public void onComplete(Result arg0) {
+                    progress.dismiss();
+                }
+            });
+
+        } else {
+            progress.dismiss();
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
+
+    // FACEBOOK LOGIN
+    private void onFbClickLogin() {
+
+        Session session = Session.getActiveSession();
+
+        if (session == null) {
+            session = new Session(this);
+
+            Session.setActiveSession(session);
+            if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
+                session.openForRead(new Session.OpenRequest(this).setCallback(this));
+            }
+
+        }
+        if (!session.isOpened() && !session.isClosed()) {
+            session.openForRead(new Session.OpenRequest(this).setCallback(this));
+        }
+    }
+
+    // QB CALLBACK
+
+    @Override
+    public void onComplete(Result arg0) {
+    }
+
+    @Override
+    public void onComplete(Result result, Object context) {
+
+        if (result.isSuccess()) {
+
+            QBUser qbUser = ((QBUserResult) result).getUser();
+            SharedPreferencesHelper.setLogin(qbUser.getLogin());
+
+            if (context.toString().equals("social")) {
+                try {
+                    qbUser.setPassword(BaseService.getBaseService().getToken());
+                } catch (BaseServiceException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } else {
+                SharedPreferencesHelper.setPassword(qbUser.getPassword());
+            }
+
+
+            loadMainScreen();
+        }
+
+
+    }
+
+    // FACEBOOK CALLBACK
+    @Override
+    public void call(Session session, SessionState state, Exception exception) {
+        QBUsers.signInUsingSocialProvider(QBProvider.FACEBOOK, session.getAccessToken(), null, this, "social");
+
+        System.out.println("token = " + session.getAccessToken());
+
+        progress = ProgressDialog.show(this, getResources().getString(R.string.app_name), getResources().getString(R.string.splash_progressdialog), true);
+
+    }
+
+    private void loadMainScreen() {
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+    // INTERNET REVIEW
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        Toast.makeText(SplashActivity.this, getResources().getString(R.string.splash_internet_error), Toast.LENGTH_LONG).show();
+        return false;
+    }
+
 }
