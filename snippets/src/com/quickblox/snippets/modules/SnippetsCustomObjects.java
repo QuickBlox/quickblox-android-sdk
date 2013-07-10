e79e4e0d9dc8c23beaa4c0eef0e4c0b567d72509
Andrew Dmitrenko <grenka487@gmail.com>
2013-07-10 16:15:27 +0300

    add permissions for customObject snippets

--- snippets/src/com/quickblox/snippets/modules/SnippetsCustomObjects.java
+++ snippets/src/com/quickblox/snippets/modules/SnippetsCustomObjects.java
@@ -7,7 +7,10 @@ import com.quickblox.internal.core.helper.StringifyArrayList;
 import com.quickblox.internal.module.custom.request.QBCustomObjectRequestBuilder;
 import com.quickblox.module.custom.QBCustomObjects;
 import com.quickblox.module.custom.model.QBCustomObject;
+import com.quickblox.module.custom.model.QBPermissions;
+import com.quickblox.module.custom.model.QBPermissionsLevel;
 import com.quickblox.module.custom.result.QBCustomObjectLimitedResult;
+import com.quickblox.module.custom.result.QBCustomObjectPermissionResult;
 import com.quickblox.module.custom.result.QBCustomObjectResult;
 import com.quickblox.module.custom.result.QBCustomObjectTaskResult;
 import com.quickblox.snippets.Snippet;
@@ -38,6 +41,7 @@ public class SnippetsCustomObjects extends Snippets {
         snippets.add(getCustomObjects);
         snippets.add(updateCustomObject);
         snippets.add(getGetCustomObjectsByIds);
+        snippets.add(getCustomObjectsPermissions);
 
         snippets.add(getCustomsObjectWithFilters);
     }
@@ -64,10 +68,17 @@ public class SnippetsCustomObjects extends Snippets {
     Snippet createCustomObject = new Snippet("create object") {
         @Override
         public void execute() {
+
+            className = "SuperSample";
             QBCustomObject customObject = new QBCustomObject(className);
-            customObject.put(fieldHealth, 99);
-            customObject.put(fieldPower, "android");
-            customObject.setParentId("50d9bf2d535c12344701c43a");
+//            customObject.put(fieldHealth, 99);
+//            customObject.put(fieldPower, "android");
+//            customObject.setParentId("50d9bf2d535c12344701c43a");
+            QBPermissions qbPermissions = new QBPermissions();
+            qbPermissions.setReadPermission(QBPermissionsLevel.OPEN);
+            qbPermissions.setUpdatePermission(QBPermissionsLevel.OPEN);
+            qbPermissions.setDeletePermission(QBPermissionsLevel.OPEN);
+            customObject.setPermission(qbPermissions);
 
             QBCustomObjects.createObject(customObject, new QBCallbackImpl() {
                 @Override
@@ -101,6 +112,23 @@ public class SnippetsCustomObjects extends Snippets {
                         QBCustomObjectTaskResult taskResult = (QBCustomObjectTaskResult) result;
 
                         System.out.format(">>> custom objects: " + taskResult.getCustomObjects().toString());
+                    }
+                }
+            });
+        }
+    };
+
+    Snippet getCustomObjectsPermissions = new Snippet("get custom object permission") {
+        @Override
+        public void execute() {
+            String coId = "d2b65efa357cf59000029";
+            QBCustomObjects.getObjectPermissions(className, coId, new QBCallbackImpl() {
+                @Override
+                public void onComplete(Result result) {
+                    if (result.isSuccess()) {
+                        QBCustomObjectPermissionResult qbCustomObjectPermissionResult = (QBCustomObjectPermissionResult) result;
+
+                        System.out.format(">>> custom object read permission: " + qbCustomObjectPermissionResult.getPermissions().getReadLevel());
                     }
                 }
             });
@@ -206,10 +234,16 @@ public class SnippetsCustomObjects extends Snippets {
             QBCustomObject co = new QBCustomObject();
             co.setClassName(className);
             HashMap<String, Object> fields = new HashMap<String, Object>();
-            fields.put(fieldPower, 1);
-            fields.put(fieldHealth, "android nes");
-            co.setFields(fields);
-            co.setCustomObjectId("51db14adefa3575e6400000a");
+//            fields.put(fieldPower, 1);
+//            fields.put(fieldHealth, "android nes");
+//            co.setFields(fields);
+
+            co.setCustomObjectId("51dd2b65efa357cf59000029");
+            QBPermissions qbPermissions = new QBPermissions();
+            qbPermissions.setReadPermission(QBPermissionsLevel.OPEN);
+            qbPermissions.setUpdatePermission(QBPermissionsLevel.OPEN);
+            qbPermissions.setDeletePermission(QBPermissionsLevel.OPEN);
+            co.setPermission(qbPermissions);
 
             QBCustomObjects.updateObject(co, new QBCallbackImpl() {
                 @Override
