e9ca520f1e9e0f1737a9dda5de1059c8c937948e
vfite <vadim.fite@qucikblox.com>
2013-12-10 11:36:41 +0200

    updated initializing snippet with setting custom domains

--- snippets/src/com/quickblox/snippets/InitializeSnippets.java
+++ snippets/src/com/quickblox/snippets/InitializeSnippets.java
@@ -15,6 +15,12 @@ public class InitializeSnippets {
 
         // App credentials from QB Admin Panel
         QBSettings.getInstance().fastConfigInit("99", "63ebrp5VZt7qTOv", "YavMAxm5T59-BRw");
+
+        //specify custom domains
+        //QBSettings.getInstance().setServerDomain(Config.SERVER_DOMAIN);
+        //QBSettings.getInstance().setContentBucketDomain(Config.CONTENT_DOMAIN);
+        //QBSettings.getInstance().setChatServerDomain(Config.CHAT_DOMAIN);
+        //QBSettings.getInstance().setTurnServerDomain(Config.TURN_SERVER_DOMAIN);
         QBAuth.createSession(null);
 
 
