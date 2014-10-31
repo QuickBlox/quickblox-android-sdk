e43d7fe15005c1ca9cbb67a83c83ec473b81dbd1
Igor Khomenko <igor@quickblox.com>
2014-10-31 16:05:05 +0200

    cleanup

--- sample-chat/src/com/quickblox/sample/chat/core/Chat.java
+++
@@ -1,9 +0,0 @@
-package com.quickblox.sample.chat.core;
-
-import org.jivesoftware.smack.XMPPException;
-
-public interface Chat {
-    void sendMessage(String message) throws XMPPException;
-
-    void release() throws XMPPException;
-}
--- sample-content/src/com/quickblox/content/utils/OnGetImageFileListener.java
+++
@@ -1,8 +0,0 @@
-package com.quickblox.content.utils;
-
-import java.io.File;
-
-public interface OnGetImageFileListener {
-
-    public void onGotImageFile(File imageFile);
-}
