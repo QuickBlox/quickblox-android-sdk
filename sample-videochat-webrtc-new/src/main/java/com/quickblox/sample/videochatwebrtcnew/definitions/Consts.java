6743bb1f3be4bf3ec14e5f5a002712f6c958449e
e.bogatov <e.bogatov@nitralabs.com>
2015-02-22 16:23:10 +0200

    Clear data. Refactor peer to peer calls.

--- sample-videochat-webrtc-new/src/main/java/com/quickblox/sample/videochatwebrtcnew/definitions/Consts.java
+++ sample-videochat-webrtc-new/src/main/java/com/quickblox/sample/videochatwebrtcnew/definitions/Consts.java
@@ -3,12 +3,12 @@ package com.quickblox.sample.videochatwebrtcnew.definitions;
 public class Consts {
 
     // QuickBlox credentials
-    public static final String APP_ID = "18846";
+  /*  public static final String APP_ID = "18846";
     public static final String AUTH_KEY = "64JzC2cuLkSMUq7";
-    public static final String AUTH_SECRET = "s4VCJZq4uWNer7H";
+    public static final String AUTH_SECRET = "s4VCJZq4uWNer7H";*/
 
-    /*public static final String APP_ID = "92";
+    public static final String APP_ID = "92";
     public static final String AUTH_KEY = "wJHdOcQSxXQGWx5";
-    public static final String AUTH_SECRET = "BTFsj7Rtt27DAmT";*/
+    public static final String AUTH_SECRET = "BTFsj7Rtt27DAmT";
     public static final String EMPTY_STRING = "";
 }
