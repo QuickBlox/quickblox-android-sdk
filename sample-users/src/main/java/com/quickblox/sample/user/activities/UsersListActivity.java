bf9e9a11194ae8a289387eb3db60eed821c4df7e
Roman <roman.pronin@injoit.com>
2016-02-19 13:51:59 +0200

    redesign

--- sample-users/src/main/java/com/quickblox/sample/user/activities/UsersListActivity.java
+++ sample-users/src/main/java/com/quickblox/sample/user/activities/UsersListActivity.java
@@ -3,9 +3,11 @@ package com.quickblox.sample.user.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
 
 import com.quickblox.core.QBEntityCallback;
@@ -22,10 +24,6 @@ public class UsersListActivity extends BaseActivity implements AdapterView.OnIte
 
     private UserListAdapter usersListAdapter;
     private ListView usersList;
-    private Button logOutButton;
-    private Button signInButton;
-    private Button selfEditButton;
-    private Button singUpButton;
 
     public static void start(Context context) {
         Intent intent = new Intent(context, UsersListActivity.class);
@@ -37,33 +35,30 @@ public class UsersListActivity extends BaseActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_list);
 
-        initUI();
         initUsersList();
     }
 
-    private void initUI() {
-        logOutButton = (Button) findViewById(R.id.logout_button);
-        signInButton = (Button) findViewById(R.id.sign_in_button);
-        selfEditButton = (Button) findViewById(R.id.self_edit_button);
-        singUpButton = (Button) findViewById(R.id.sign_up_button);
-        usersList = (ListView) findViewById(R.id.users_listview);
-    }
-
     private void initUsersList() {
+        usersList = (ListView) findViewById(R.id.users_listview);
+
         usersListAdapter = new UserListAdapter(this);
         usersList.setAdapter(usersListAdapter);
         usersList.setOnItemClickListener(this);
     }
 
+    private void setTitle(boolean signIn) {
+        if (signIn) {
+            getSupportActionBar().setTitle(R.string.singed_in);
+        } else {
+            getSupportActionBar().setTitle(R.string.not_singed_in);
+        }
+    }
+
     @Override
     public void onResume() {
         super.onResume();
-        if (DataHolder.getDataHolder().getSignInQbUser() != null) {
-            signInButton.setVisibility(View.GONE);
-            singUpButton.setVisibility(View.GONE);
-            logOutButton.setVisibility(View.VISIBLE);
-            selfEditButton.setVisibility(View.VISIBLE);
-        }
+        setTitle(DataHolder.getDataHolder().getSignInQbUser() != null);
+
         usersListAdapter.notifyDataSetChanged();
     }
 
@@ -74,63 +69,9 @@ public class UsersListActivity extends BaseActivity implements AdapterView.OnIte
         DataHolder.getDataHolder().setSignInQbUser(null);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            signInButton.setVisibility(View.GONE);
-            logOutButton.setVisibility(View.VISIBLE);
-        }
-    }
-
-    public void onClick(View view) {
-        Intent intent;
-        switch (view.getId()) {
-        case R.id.sign_in_button:
-            intent = new Intent(this, SignInActivity.class);
-            startActivityForResult(intent, 0);
-            break;
-
-        case R.id.sign_up_button:
-            intent = new Intent(this, SignUpUserActivity.class);
-            startActivity(intent);
-            break;
-
-        case R.id.logout_button:
-            progressDialog.show();
-
-            // Logout
-            //
-            QBUsers.signOut(new QBEntityCallback<Void>() {
-                @Override
-                public void onSuccess(Void result, Bundle bundle) {
-                    progressDialog.hide();
-
-                    Toaster.longToast(R.string.user_log_out_msg);
-                    updateDataAfterLogOut();
-                }
-
-                @Override
-                public void onError(QBResponseException e) {
-                    progressDialog.hide();
-
-                    Toaster.longToast(e.getErrors().toString());
-                }
-            });
-            break;
-
-        case R.id.self_edit_button:
-            intent = new Intent(this, UpdateUserActivity.class);
-            startActivity(intent);
-            break;
-        }
-    }
-
     private void updateDataAfterLogOut() {
         DataHolder.getDataHolder().setSignInQbUser(null);
-        signInButton.setVisibility(View.VISIBLE);
-        logOutButton.setVisibility(View.GONE);
-        selfEditButton.setVisibility(View.GONE);
-        singUpButton.setVisibility(View.VISIBLE);
+        invalidateOptionsMenu();
     }
 
     @Override
@@ -142,5 +83,60 @@ public class UsersListActivity extends BaseActivity implements AdapterView.OnIte
         Intent intent = new Intent(this, ShowUserActivity.class);
         intent.putExtra(POSITION, position);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_users_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (DataHolder.getDataHolder().getSignInQbUser() == null) {
+            menu.getItem(2).setEnabled(false);
+            setTitle(false);
+        } else {
+            setTitle(true);
+            menu.getItem(2).setEnabled(true);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.sign_in:
+                intent = new Intent(this, SignInActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.sign_up:
+                intent = new Intent(this, SignUpUserActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.logout:
+                progressDialog.show();
+                QBUsers.signOut(new QBEntityCallback<Void>() {
+                    @Override
+                    public void onSuccess(Void result, Bundle bundle) {
+                        progressDialog.hide();
+                        Toaster.longToast(R.string.user_log_out_msg);
+                        updateDataAfterLogOut();
+                    }
+
+                    @Override
+                    public void onError(QBResponseException e) {
+                        progressDialog.hide();
+
+                        Toaster.longToast(e.getErrors().toString());
+                    }
+                });
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
