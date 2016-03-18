df6db522a1f8fce3c0bc0586c929cc2a0acb7601
Roman <roman.pronin@injoit.com>
2016-03-18 14:31:13 +0200

    update snackbarError CoreBaseActivity

--- sample-core/src/main/java/com/quickblox/sample/core/utils/ErrorUtils.java
+++ sample-core/src/main/java/com/quickblox/sample/core/utils/ErrorUtils.java
@@ -16,22 +16,47 @@ import java.util.List;
 
 public class ErrorUtils {
 
+    private static final String NO_CONNECTION_ERROR = "Connection failed. Please check your internet connection.";
+    private static final String NO_RESPONSE_TIMEOUT = "No response received within reply timeout.";
     private static Handler mainThreadHandler = new Handler(Looper.getMainLooper());
 
-    private ErrorUtils() {}
+    private ErrorUtils() {
+    }
 
-    public static void showSnackbar(View view, @StringRes int errorMessage, List<String> errors,
+    public static void showSnackbar(View view, @StringRes int errorMessage, Exception e,
+                                    @StringRes int actionLabel, View.OnClickListener clickListener) {
+        String error = e.getMessage();
+        boolean noConnection = NO_CONNECTION_ERROR.equals(error);
+        boolean timeout = error.startsWith(NO_RESPONSE_TIMEOUT);
+        if (noConnection || timeout) {
+            showSnackbar(view, R.string.no_internet_connection, actionLabel, clickListener);
+        } else {
+            showSnackbar(view, errorMessage, error, actionLabel, clickListener);
+        }
+    }
+
+    public static void showSnackbar(View view, @StringRes int errorMessage, String error,
                                     @StringRes int actionLabel, View.OnClickListener clickListener) {
         String errorMessageString = CoreApp.getInstance().getString(errorMessage);
-        String message = String.format("%s: %s", errorMessageString, errors.toString());
+        String message = String.format("%s: %s", errorMessageString, error);
         showSnackbar(view, message, actionLabel, clickListener);
     }
 
-    private static void showSnackbar(View view, String message,
-                                    @StringRes int actionLabel,
-                                    View.OnClickListener clickListener) {
+    private static void showSnackbar(View view, @StringRes int message,
+                                     @StringRes int actionLabel,
+                                     View.OnClickListener clickListener) {
         Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE);
         snackbar.setAction(actionLabel, clickListener);
+        snackbar.show();
+    }
+
+    private static void showSnackbar(View view, String message,
+                                     @StringRes int actionLabel,
+                                     View.OnClickListener clickListener) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE);
+        if (clickListener != null) {
+            snackbar.setAction(actionLabel, clickListener);
+        }
         snackbar.show();
     }
 
