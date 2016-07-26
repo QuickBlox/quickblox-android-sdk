ae921158b1f48b4dd0fabd7fcb5795c1a75e1d5d
Roman Pronin <sabak-barabak@yandex.com>
2016-07-26 12:14:50 +0300

    minor refactor chat module

--- sample-chat/src/main/java/com/quickblox/sample/chat/utils/chat/ChatHelper.java
+++ sample-chat/src/main/java/com/quickblox/sample/chat/utils/chat/ChatHelper.java
@@ -152,10 +152,10 @@ public class ChatHelper {
 
     public void deleteDialog(QBDialog qbDialog, QBEntityCallback<Void> callback) {
         if (qbDialog.getType() == QBDialogType.GROUP) {
-            QBChatService.getInstance().getGroupChatManager().deleteDialog(qbDialog.getDialogId(),
+            QBChatService.getInstance().getGroupChatManager().deleteDialog(qbDialog.getDialogId(), false,
                     new QbEntityCallbackWrapper<>(callback));
         } else if (qbDialog.getType() == QBDialogType.PRIVATE) {
-            QBChatService.getInstance().getPrivateChatManager().deleteDialog(qbDialog.getDialogId(),
+            QBChatService.getInstance().getPrivateChatManager().deleteDialog(qbDialog.getDialogId(), false,
                     new QbEntityCallbackWrapper<>(callback));
         } else if (qbDialog.getType() == QBDialogType.PUBLIC_GROUP) {
             Toaster.shortToast(R.string.public_group_chat_cannot_be_deleted);
@@ -251,7 +251,7 @@ public class ChatHelper {
 
     public void getUsersFromDialog(QBDialog dialog,
                                    final QBEntityCallback<ArrayList<QBUser>> callback) {
-        ArrayList<Integer> userIds = dialog.getOccupants();
+        List<Integer> userIds = dialog.getOccupants();
 
         ArrayList<QBUser> users = new ArrayList<>(userIds.size());
         for (Integer id : userIds) {
