1244df0fab4ec992e3bd596709273ca00b518e34
tatanka987 <tatanka.tvv@gmail.com>
2017-12-11 15:22:23 +0200

    - update SDT to 3.5.2; - fixed ANDSAMPLES-344;

--- sample-chat/src/main/java/com/quickblox/sample/chat/ui/adapter/DialogsAdapter.java
+++ sample-chat/src/main/java/com/quickblox/sample/chat/ui/adapter/DialogsAdapter.java
@@ -60,7 +60,7 @@ public class DialogsAdapter extends BaseSelectableListAdapter<QBChatDialog> {
             holder.unreadCounterTextView.setVisibility(View.GONE);
         } else {
             holder.unreadCounterTextView.setVisibility(View.VISIBLE);
-            holder.unreadCounterTextView.setText(String.valueOf(unreadMessagesCount > 99 ? 99 : unreadMessagesCount));
+            holder.unreadCounterTextView.setText(String.valueOf(unreadMessagesCount > 99 ? "99+" : unreadMessagesCount));
         }
 
         holder.rootLayout.setBackgroundColor(isItemSelected(position) ? ResourceUtils.getColor(R.color.selected_list_item_color) :
