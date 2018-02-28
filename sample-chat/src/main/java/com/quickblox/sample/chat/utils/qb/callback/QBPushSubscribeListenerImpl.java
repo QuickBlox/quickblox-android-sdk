6eb82463e9db2c0b54fc6160b922ccc6a08f8876
Roman Pronin <roman.pronin@injoit.com>
2018-02-28 12:56:17 +0200

    add logoutREST

---
+++ sample-chat/src/main/java/com/quickblox/sample/chat/utils/qb/callback/QBPushSubscribeListenerImpl.java
@@ -0,0 +1,25 @@
+package com.quickblox.sample.chat.utils.qb.callback;
+
+import com.quickblox.messages.services.QBPushManager;
+
+/**
+ * Created by roman on 2/28/18.
+ */
+
+public class QBPushSubscribeListenerImpl implements QBPushManager.QBSubscribeListener {
+
+    @Override
+    public void onSubscriptionCreated() {
+
+    }
+
+    @Override
+    public void onSubscriptionError(Exception e, int i) {
+
+    }
+
+    @Override
+    public void onSubscriptionDeleted(boolean b) {
+
+    }
+}
