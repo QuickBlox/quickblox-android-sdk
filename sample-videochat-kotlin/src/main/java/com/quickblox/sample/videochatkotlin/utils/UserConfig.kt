fc14dcac308ff55f7419e4538c9059f63da05ed4
Roman Pronin <roman.pronin@injoit.com>
2018-05-03 18:44:58 +0300

    fix json parsing prior v. 20 API

--- sample-videochat-kotlin/src/main/java/com/quickblox/sample/videochatkotlin/utils/UserConfig.kt
+++ sample-videochat-kotlin/src/main/java/com/quickblox/sample/videochatkotlin/utils/UserConfig.kt
@@ -1,10 +1,10 @@
 package com.quickblox.sample.videochatkotlin.utils
 
-import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.JsonElement
 import com.quickblox.sample.core.utils.configs.ConfigParser
 import com.quickblox.users.model.QBUser
 import org.json.JSONException
-import org.json.JSONObject
 
 
 /**
@@ -13,12 +13,14 @@ import org.json.JSONObject
 
 fun getAllUsersFromFile(fileName: String): ArrayList<QBUser> {
     val qbUsers = ArrayList<QBUser>()
+    val json = Gson().fromJson(ConfigParser().getConfigsAsJsonString(fileName), JsonElement::class.java)
     try {
-        val json = ConfigParser().getConfigsAsJson(fileName)
-        val keys = json.keys()
-        while (keys.hasNext()) {
-            val loginField = keys.next()
-            val qbUser = QBUser(loginField, json.getString(loginField))
+        val jsonObject = json.asJsonObject
+        val iterator = jsonObject.keySet().iterator()
+        while (iterator.hasNext()) {
+            val loginField = iterator.next()
+            val password = jsonObject.get(loginField).asString
+            val qbUser = QBUser(loginField, password)
             qbUsers.add(qbUser)
         }
     } catch (e: JSONException) {
