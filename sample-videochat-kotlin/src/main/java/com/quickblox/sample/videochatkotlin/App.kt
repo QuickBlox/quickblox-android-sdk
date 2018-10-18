156dd3789e25c027583652c84ca6b077e328edfa
Kirill.Tolmachev <kirill.tolmachev@quickblox.com>
2018-10-18 15:18:50 +0300

    1: added support qb_config.json file 2: refactored code 3: delete excess logic from App 4: modified layout activity_login.xml 5: modified string resource

--- sample-videochat-kotlin/src/main/java/com/quickblox/sample/videochatkotlin/App.kt
+++ sample-videochat-kotlin/src/main/java/com/quickblox/sample/videochatkotlin/App.kt
@@ -1,43 +1,5 @@
 package com.quickblox.sample.videochatkotlin
 
-import android.app.Application
-import com.quickblox.auth.session.QBSettings
-import com.quickblox.sample.videochatkotlin.utils.SAMPLE_CONFIG_FILE_NAME
-import com.quickblox.sample.videochatkotlin.utils.getAllUsersFromFile
-import com.quickblox.users.model.QBUser
+import com.quickblox.sample.core.CoreApp
 
-class App : Application() {
-    private val applicationID = ""
-    private val authKey = ""
-    private val authSecret = ""
-    private val accountKey = ""
-
-    override fun onCreate() {
-        super.onCreate()
-        checkQBConfigJson()
-        checkUserJson()
-        initCredentials()
-    }
-
-    private fun checkQBConfigJson() {
-        if (applicationID.isEmpty() || authKey.isEmpty() || authSecret.isEmpty() || accountKey.isEmpty()) {
-            throw AssertionError(getString(R.string.error_qb_credentials_empty))
-        }
-    }
-
-    private fun checkUserJson() {
-        val users = getAllUsersFromFile(SAMPLE_CONFIG_FILE_NAME, this)
-        if (users.size !in 2..4 || isUsersEmpty(users))
-            throw AssertionError(getString(R.string.error_users_empty))
-    }
-
-    private fun isUsersEmpty(users: ArrayList<QBUser>): Boolean {
-        users.forEach { user -> if (user.login.isBlank() || user.password.isBlank()) return true }
-        return false
-    }
-
-    private fun initCredentials() {
-        QBSettings.getInstance().init(applicationContext, applicationID, authKey, authSecret)
-        QBSettings.getInstance().accountKey = accountKey
-    }
-}
+class App : CoreApp()
