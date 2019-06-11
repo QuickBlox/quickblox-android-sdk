87908d749685f84cb533280f50bc08f01dce35f3
Egor Pavlov <egor.popov@quickblox.com>
2019-06-11 15:58:07 +0300

    uploaded "sample-chat-kotlin"

--- sample-chat-java/src/main/java/com/quickblox/sample/chat/java/ui/adapter/listeners/AttachClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.quickblox.sample.chat.java.ui.adapter.listeners;
-
-import com.quickblox.chat.model.QBAttachment;
-
-public interface AttachClickListener {
-
-    void onLinkClicked(QBAttachment attachment, int positionInAdapter);
-}
