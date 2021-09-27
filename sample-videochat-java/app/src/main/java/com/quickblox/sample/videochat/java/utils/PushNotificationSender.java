e68a58a34d0402df2deb17456b10fcc400e8e0b8
Dovbnya Vitaliy <vitaliy.dovbnya@quickblox.com>
2021-09-27 19:15:05 +0300

    4.0.4-videochat-java 4.0.4-videochat-kotlin

--- sample-videochat-java/app/src/main/java/com/quickblox/sample/videochat/java/utils/PushNotificationSender.java
+++ sample-videochat-java/app/src/main/java/com/quickblox/sample/videochat/java/utils/PushNotificationSender.java
@@ -10,25 +10,14 @@ import com.quickblox.sample.videochat.java.R;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-
 
 public class PushNotificationSender {
-
-    public static void sendPushMessage(ArrayList<Integer> recipients,
-                                       String senderName,
-                                       String newSessionID,
-                                       String opponentsIDs,
-                                       String opponentsNames,
-                                       boolean isVideoCall) {
+    public static void sendPushMessage(ArrayList<Integer> recipients, String senderName, String newSessionID,
+                                       String opponentsIDs, String opponentsNames, boolean isVideoCall) {
         String outMessage = String.format(String.valueOf(R.string.text_push_notification_message), senderName);
 
-        Date currentTime = Calendar.getInstance().getTime();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String eventDate = simpleDateFormat.format(currentTime);
+        long timeStamp = System.currentTimeMillis();
 
         // Send Push: create QuickBlox Push Notification Event
         QBEvent qbEvent = new QBEvent();
@@ -45,7 +34,7 @@ public class PushNotificationSender {
             json.put("opponentsIDs", opponentsIDs);
             json.put("contactIdentifier", opponentsNames);
             json.put("conferenceType", isVideoCall ? "1" : "2");
-            json.put("timestamp", eventDate);
+            json.put("timestamp", Long.toString(timeStamp));
         } catch (JSONException e) {
             e.printStackTrace();
         }
